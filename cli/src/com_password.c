e45068f68f416238be6b204d22781fe9d4c3b06f
darkcoven <admin@darkcoven.tk>
2013-10-18 23:31:40 +0200

    Cli: change password can ask new password with terminal echo disabled

--- cli/src/com_password.c
+++ cli/src/com_password.c
@@ -5,11 +5,13 @@
 int do_password_change (int argc, const char **argv, struct ngadmin *nga)
 {
 	int i;
+	char buf[64];
 	const struct swi_attr *sa;
+	const char *pass;
 	
 	
-	if (argc != 1) {
-		printf("usage: password change <value>\n");
+	if (argc > 1) {
+		printf("usage: password change [<value>]\n");
 		return 1;
 	}
 	
@@ -19,8 +21,24 @@ int do_password_change (int argc, const char **argv, struct ngadmin *nga)
 		return 1;
 	}
 	
-	i = ngadmin_changePassword(nga, argv[0]);
-	printErrCode(i);
+	if (argc == 0) {
+		printf("Enter new password: ");
+		fflush(stdout);
+		current_term.c_lflag &= ~ECHO;
+		tcsetattr(STDIN_FILENO, TCSANOW, &current_term);
+		pass = fgets(buf, sizeof(buf), stdin);
+		trim(buf, strlen(buf));
+		current_term.c_lflag |= ECHO;
+		tcsetattr(STDIN_FILENO, TCSANOW, &current_term);
+		putchar('\n');
+	} else {
+		pass = argv[0];
+	}
+	
+	if (pass != NULL) {
+		i = ngadmin_changePassword(nga, pass);
+		printErrCode(i);
+	}
 	
 	
 	return 0;
